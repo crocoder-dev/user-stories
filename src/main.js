b5d75bb1280bd33de6779df07ee247b38806cd26
Luka <luka.sesar99@gmail.com>
2023-06-28 16:57:01 +0200

    Feat: added spelling package and functions that show which words are misspelled

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import { Schema, DOMParser } from "prosemirror-model"
 import { schema } from "prosemirror-schema-basic"
 import { addListNodes } from "prosemirror-schema-list"
 import { exampleSetup } from "prosemirror-example-setup"
+import spelling from "spelling";
+import en_us from 'spelling/dictionaries/en_US'
 
 import { simpleUserStory, complexUserStory } from "./user-stories";
 
+const dict = new spelling(en_us);
+dict.insert('is')
 
 const mySchema = new Schema({
   nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
@@ -29,9 +33,12 @@ const stories = [
 const app = document.querySelector('#app');
 
 const printUserStory = () => {
-  const { userStory, content, html } = getUserStory();
-  console.log('content', content);
-  console.log('html', html);
+  const { userStory, content, html, title } = getUserStory();
+
+  checker(content);
+  console.log('title:', title);
+  console.log('content:', content);
+  console.log('html:', html);
 }
 
 const printUserStoryButton = document.createElement('button');
@@ -50,10 +57,33 @@ const getUserStory = () => {
   const userStory = document.querySelector('.ProseMirror'); 
   const content = userStory.textContent;
   const html = userStory.innerHTML;
+  const title = document.querySelector('#title').value;
 
   return {
     userStory,
     content,
     html,
+    title
   };
 };
+
+const checker = (text) => {
+  const errorsDiv = document.querySelector('.errors');
+  text.trim().split(' ').forEach(word => {
+    const clearedWord = word.replace('[^a-zA-Z0-9]', '')
+    const checkWord = dict.lookup(clearedWord)
+    if (!checkWord.found) {
+      const pEl = document.createElement('p');
+      pEl.innerText = `Warning => Word '${word}' misspelled. ${checkWord.suggestions.length ? 'Suggestions: ' + checkWord.suggestions.map(e => e.word).join(', ') : 'No suggestions'}.`;
+      errorsDiv.appendChild(pEl);
+      underlineWord(word);
+    }
+  })
+}
+
+function underlineWord(word) {
+  const editor = document.getElementById("editor");
+  const text = editor.innerHTML;
+  const newText = text.replace(new RegExp('\\b' + word + '\\b', 'gi'), '<span class="red">$&</span>');
+  editor.innerHTML = newText;
+}
